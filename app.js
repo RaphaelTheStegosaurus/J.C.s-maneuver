38644b0fe145a20b63c6ab3e3d8f62f17954351f
RaphaelTheStegosaurus <r.a.s.m.13@hotmail.com>
2025-11-11 13:57:42 -0800

    Adding a orbit basic

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ class Player extends EngineObject {
     this.color = WHITE;
     this.tileInfo = new TileInfo(vec2(0, 0), vec2(32, 32), 0);
     this.angleDamping = 0.95;
+    // this.velocity = vec2(0.01, 0.01);
     console.log(this);
   }
   update() {
@@ -56,15 +57,36 @@ class Player extends EngineObject {
   }
 }
 class Planet extends EngineObject {
-  constructor(pos, size) {
+  constructor(pos, size, player) {
     super(pos, size);
     this.color = WHITE;
     this.setCollision();
+    this.Player = player;
+    this.GRAVITY_RANGE = 20;
+    this.GRAVITY_STRENGTH = 0.0001;
+  }
+  update() {
+    this.applyGravity();
+    super.update();
+  }
+  applyGravity() {
+    const delta = this.pos.subtract(this.Player.pos);
+    const distance = delta.length();
+    if (distance > 0 && distance < this.GRAVITY_RANGE) {
+      const direction = delta.normalize();
+      const forceMagnitude = this.GRAVITY_STRENGTH / distance;
+      const gravityAcceleration = direction.scale(forceMagnitude);
+      this.Player.velocity = this.Player.velocity.add(gravityAcceleration);
+      debugText(
+        `Distance: ${distance.toFixed(2)}`,
+        vec2(this.Player.pos.x, this.Player.pos.y + 3)
+      );
+    }
   }
 }
 function gameInit() {
-  new Player(vec2(0, 0), vec2(1, 2));
-  new Planet(vec2(10, 10), vec2(1, 1));
+  const player = new Player(vec2(0, 0), vec2(1, 2));
+  new Planet(vec2(10, 10), vec2(1, 1), player);
 }
 function gameUpdate() {}
 function gameUpdatePost() {}
